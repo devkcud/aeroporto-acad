63a92c71ffb3f12efd4abbe6fd8b7fe077e06685
André devkcud Albanese <patommmmm@proton.me>
2023-11-29 22:00:32 -0300

    feat: Add actions (remove & insert)

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,28 @@
 <?php
-  require_once("config.php");
+  require_once("../config.php");
   $con = connectDB();
+
+  if (isset($_GET["insert"])) {
+    $address = $_GET["insert"];
+
+    $stmt = $con->prepare("INSERT INTO Airport (Address) VALUES (?)");
+    $stmt->bind_param("s", $address);
+    $stmt->execute();
+    $stmt->close();
+
+    header("Location: home.php");
+  }
+
+  if (isset($_GET["remove"])) {
+    $id = $_GET["remove"];
+
+    $stmt = $con->prepare("DELETE FROM Airport WHERE AirportID = ?");
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
+    $stmt->close();
+
+    header("Location: home.php");
+  }
 
   $result = $con->query("SELECT * FROM Airport");
 ?>
@@ -14,8 +36,8 @@
     <title>Aeroporto ACAD</title>
   </head>
   <body>
-    <form action="registar.php" method="POST">
-      <input type="text" name="address" placeholder="Endereço">
+    <form method="GET">
+      <input type="text" name="insert" placeholder="Endereço">
 
       <button>Registar</button>
     </form>
@@ -27,8 +49,14 @@
         while($row = $result->fetch_assoc()) {
           echo "<li>";
           echo "<div class='address'>";
-          echo "<img src='img/map.svg' width='40px' height='40px' />";
+          echo "<img src='../img/map.svg' width='40px' height='40px' />";
           echo "<span>" . $row["Address"] . "</span>";
+          echo "</div>";
+          echo "<div class='actions'>";
+          echo "<form method='GET'>";
+          echo "<input type='hidden' name='remove' value='" . $row["AirportID"] . "' />";
+          echo "<button type='submit'>Remover</button>";
+          echo "</form>";
           echo "</div>";
           echo "</li>";
         }
